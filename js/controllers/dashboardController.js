66e7270e71bf5889af7a4701f0bc32cc4c49f77e
claudio <clo.santoro@hotmail.it>
2019-11-18 18:48:13 +0100

    dati utente

--- js/controllers/dashboardController.js
+++ js/controllers/dashboardController.js
@@ -1,4 +1,4 @@
-function getNumberUsers(emailVal) {
+function getNumberUsers() {
     console.log('called number users');
 
     //MODIFICARE PER AWS
@@ -24,6 +24,51 @@ function getNumberUsers(emailVal) {
             var nweText = text;
             this.count = JSON.parse(nweText).number;
             console.log('number is: ' + this.count);
+            return this.count;
+        });
+    }).catch(function(error) {
+        console.log("error is: " + error);
+        var date = new Date();
+        var curDate = null;
+        do { curDate = new Date(); }
+        while(curDate-date < 5000)
+        return error;
+    });
+
+    console.log('ended fetch!Results: ' + results);
+
+
+};
+
+
+
+
+function getTotalTeachers() {
+    console.log('called number users');
+
+    //MODIFICARE PER AWS
+    var uri = "http://localhost:8080/users/teachers/";
+    var results;
+    var count;
+
+    fetch(uri, {
+        method: 'GET',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        console.log('RES: ' + JSON.stringify(response));
+
+
+        response.text().then(function (text) {
+
+            console.log('text is: ' + text);
+
+            var nweText = text;
+            this.count = JSON.parse(nweText).number;
+            console.log('number teachers is: ' + this.count);
             return this.count;
         });
     }).catch(function(error) {
