7cb9ed1e0e8cf5e953434393d8a646efdabeab8f
claudio <clo.santoro@hotmail.it>
2019-11-20 18:42:02 +0100

    aggiustato doppio click

---
+++ js/controllers/logiController.js
@@ -0,0 +1,98 @@
+console.log('trying prova');
+
+//prova();
+var uri_userLogin = "http://localhost:8080/users/login";
+
+function nameFunc(val1,val2){
+
+    console.log('nameFunc!');
+
+    var nweText;
+    var name;
+    var jwtToken;
+    var email;
+    var username;
+    var surname;
+    var role;
+
+
+   // var x = 0;
+
+    //if(x === 0) {
+      //  console.log('x is 0');
+       // window.location = '/dashboard_2.html?email=';
+    //}
+
+
+    fetch(uri_userLogin, {
+        method: 'POST',
+        body: JSON.stringify({
+            em: val1,
+            pass: val2
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        console.log("getting response:  " +JSON.stringify(response));
+        console.log('RES status: ' + JSON.stringify(response.status));
+        console.log('status:' + response.status);
+
+//            if(response.status === 200){
+
+        response.text().then(function (text) {
+            console.log('text is: ' + text);
+
+            nweText = text;
+            name = JSON.parse(nweText).name;
+            jwtToken = JSON.parse(nweText).token;
+            email = JSON.parse(nweText).email;
+            username = JSON.parse(nweText).username;
+            surname = JSON.parse(nweText).surname;
+            role = JSON.parse(nweText).role;
+
+            console.log('got name: ' + name);
+            console.log('username: ' + username);
+            console.log('email:' + email);
+            console.log('surname: ' + surname);
+            console.log('role: ' + role);
+
+
+            //SETTING AUTHINFO
+            /************************************************************************/
+            sessionStorage.setItem('logged', username);
+            sessionStorage.setItem(username, jwtToken);
+            sessionStorage.setItem('email',email);
+            console.log('setted email: ' + sessionStorage.getItem('email'));
+            sessionStorage.setItem('name',name);
+            sessionStorage.setItem('surname',surname);
+            sessionStorage.setItem('role',role);
+            /***********************************************************************/
+
+            //var start = new Date().getTime();
+            //while (new Date().getTime() < start + 3000);
+
+
+            window.location='/dashboard_2.html?email='+email;
+
+
+        });
+
+
+    }).catch(function (e) {
+        console.log('There was an error');
+    });
+
+
+
+    console.log('insidee postNameFunc; uri is:' + uri_userLogin);
+
+    //var start = new Date().getTime();
+    //while (new Date().getTime() < start + 3000);
+    //return false;
+};
+
+
+
--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -9,7 +9,7 @@ function postNameFunc(val1,val2){
 
     console.log('inside postNameFunc; uri is:' + uri_userLogin);
         //console.log("makePostNameFunc; uri: " + JSON.stringify(uri));
-        fetch(uri_userLogin, {
+    fetch(uri_userLogin, {
             method: 'POST',
             body: JSON.stringify({
                 em: val1,
@@ -25,34 +25,69 @@ function postNameFunc(val1,val2){
             console.log('RES status: ' + JSON.stringify(response.status));
             console.log('status:' + response.status);
 
-            if(response.status === 200){
+//            if(response.status === 200){
 
                 response.text().then(function (text) {
                     console.log('text is: ' + text);
 
+
                     var nweText = text;
                     var name = JSON.parse(nweText).name;
                     var jwtToken = JSON.parse(nweText).token;
+                    var email = JSON.parse(nweText).email;
+                    var username = JSON.parse(nweText).username;
+                    var surname = JSON.parse(nweText).surname;
+                    var role = JSON.parse(nweText).role;
+
+                    console.log('got name: ' + name);
+                    console.log('username: ' + username);
+                    console.log('email:' + email);
+                    console.log('surname: ' + surname);
+                    console.log('role: ' + role);
 
                     /****************setting authinfo************/
-                    sessionStorage.setItem('logged',name);
-                    sessionStorage.setItem(name,jwtToken);
+
+                    //if(name !== null && name != undefined) {
+                        sessionStorage.setItem('logged', username);
+                        sessionStorage.setItem(username, jwtToken);
+                        sessionStorage.setItem('email',email);
+                        console.log('setted email: ' + sessionStorage.getItem('email'));
+                        sessionStorage.setItem('name',name);
+                        sessionStorage.setItem('surname',surname);
+                        sessionStorage.setItem('role',role);
+
+                        var start = new Date().getTime();
+                        while (new Date().getTime() < start + 3000);
+                    //}
+
                     /********************************************/
 
 
-                    console.log('token is: ' + sessionStorage.getItem(name));
-
-
-                    window.location='/dashboard_2.html?email='+name;
+                    //console.log('token is: ' + sessionStorage.getItem('logged'));
 
                 });
-                console.log('not 200');
-                window.location='/login.html';
 
-            }console.log('not 200');
+            console.log('going to dashboard: ' + email);
+
+      //      var start = new Date().getTime();
+      //      while (new Date().getTime() < start + 3000);
+
+            window.location='/dashboard_2.html?email='+sessionStorage.getItem('email');
+            return false;
+
+            //}
+        }).catch(function (e) {
+            console.log('There was an error');
         });
 
-        console.log('ended');
+    /*var start = new Date().getTime();
+    while (new Date().getTime() < start + 3000);*/
+    console.log('ended');
+
+    //var start = new Date().getTime();
+    //while (new Date().getTime() < start + 3000);
+
+    return false;
 };
 
 
